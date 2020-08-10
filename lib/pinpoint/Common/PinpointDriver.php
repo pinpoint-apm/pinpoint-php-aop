128ecddfd7020b81cdc40678d23a4cff6fda6b57
eeliu <eeliu2009@gmail.com>
2020-08-10 15:01:04 +0800

    update licence

--- lib/pinpoint/Common/PinpointDriver.php
+++ lib/pinpoint/Common/PinpointDriver.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2019 NAVER Corp.
+ * Copyright 2020-present NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
