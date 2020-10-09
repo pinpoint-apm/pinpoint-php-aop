3ccb19b4d70fbf2760c3725f73dcc881a841bf8a
eeliu <eeliu2009@gmail.com>
2020-10-09 17:20:40 +0800

    fix a bug when no $classMap definition

--- auto_pinpointed.php
+++ auto_pinpointed.php
@@ -4,10 +4,11 @@ namespace pinpoint;
 use pinpoint\Common\AopClassMap;
 use pinpoint\Common\PinpointDriver;
 
-$classMap = null;
+$classMap = new AopClassMap();
 if(defined('USER_DEFINED_CLASS_MAP_IMPLEMENT'))
 {
     $className = USER_DEFINED_CLASS_MAP_IMPLEMENT;
+    global $classMap;
     $classMap = new $className();
     assert($classMap instanceof AopClassMap);
 }
