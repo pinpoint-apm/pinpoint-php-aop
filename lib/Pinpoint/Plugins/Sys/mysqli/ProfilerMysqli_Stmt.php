c8dd9d61c1bab0614fd2020ba40db3cbde721bb9
eeliu <27064129+eeliu@users.noreply.github.com>
2022-01-11 15:05:21 +0800

    ProfilerMysqli_Stmt in php8.1

--- lib/Pinpoint/Plugins/Sys/mysqli/ProfilerMysqli_Stmt.php
+++ lib/Pinpoint/Plugins/Sys/mysqli/ProfilerMysqli_Stmt.php
@@ -3,12 +3,13 @@
  * User: eeliu
  * Date: 11/5/20
  * Time: 5:32 PM
+ * changes: 2022-1-11: php8.1, the only way is extends all function
  */
 
 namespace Pinpoint\Plugins\Sys\mysqli;
 
 
-class ProfilerMysqli_Stmt
+class ProfilerMysqli_Stmt extends \mysqli_stmt
 {
     protected $_instance;
     public function __construct(&$instance)
@@ -16,30 +17,17 @@ class ProfilerMysqli_Stmt
         $this->_instance = &$instance;
     }
 
-    public function __call($name, $arguments)
+    public function bind_result (mixed &...$vars): bool
     {
-        return call_user_func_array([&$this->_instance,$name],$arguments);
+        return $this->_instance->bind_result(...$vars);
     }
 
-    public function bind_param ($types, &$var1, &...$_)
-    {
-        $param = \pinpoint_get_func_ref_args();
-        return call_user_func_array([$this->_instance,'bind_param'],$param);
-
-    }
-
-    public function bind_result (&$var1, &...$_)
-    {
-        $param = \pinpoint_get_func_ref_args();
-        return call_user_func_array([$this->_instance,'bind_result'],$param);
-    }
-
-    public function execute()
+    public function execute(?array $params = null): bool
     {
         $plugin = new StmtExecutePlugin("Stmt::execute",$this);
         try{
             $plugin->onBefore();
-            $ret =  call_user_func_array([$this->_instance,'execute'],[]);
+            $ret =  call_user_func([$this->_instance,'execute'],$params);
             $plugin->onEnd($ret);
             return $ret;
 
@@ -49,5 +37,82 @@ class ProfilerMysqli_Stmt
         }
     }
 
+    public function attr_get(int $attribute): int
+    {
+        return call_user_func([$this->_instance,'attr_get'],$attribute);
+    }
+
+    public function attr_set(int $attribute, int $value): bool
+    {
+        return call_user_func([$this->_instance,'attr_set'],$attribute,$value);
+    }
+
+    public function bind_param(string $types, mixed &...$vars): bool
+    {
+        return $this->_instance->bind_param($types,$vars);
+    }
+
+    public function close(): bool
+    {
+        return $this->_instance->close();
+    }
+    public function data_seek(int $offset): void
+    {
+        $this->_instance->data_seek($offset);
+    }
+
+    public function fetch(): ?bool
+    {
+       return $this->_instance->fetch();
+    }
+    public function free_result(): void
+    {
+        $this->_instance->free_result();
+    }
+
+    public function get_result(): \mysqli_result|false
+    {
+        return $this->_instance->get_result();
+    }
+
+    public function get_warnings(): \mysqli_warning|false
+    {
+        return $this->_instance->get_warnings();
+    }
+
+    public function more_results(): bool
+    {
+        return $this->_instance->more_results();
+    }
+    public  function next_result(): bool
+    {
+        return $this->_instance->next_result();
+    }
+    public function num_rows(): int|string
+    {
+        return $this->_instance->num_rows();
+    }
+    public function prepare(string $query): bool
+    {
+        return $this->_instance->prepare($query);
+    }
+    public function reset(): bool
+    {
+        return $this->_instance->reset();
+    }
+    public function result_metadata(): \mysqli_result|false
+    {
+        return $this->_instance->result_metadata();
+    }
+
+    public function send_long_data(int $param_num, string $data): bool
+    {
+        return $this->_instance->send_long_data($param_num,$data);
+    }
+
+    public function store_result(): bool
+    {
+        return $this->_instance->store_result();
+    }
 
 }
